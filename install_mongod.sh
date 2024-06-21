7d30277b2eda5d20391e550704d94c3d53511ef1
Louis Maddox <louismmx@gmail.com>
2024-06-21 23:50:29 +0100

    debug

--- install_mongod.sh
+++ install_mongod.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
+set -x
 
 echo "Installing MongoDB"
-
-# Check if running as root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root or using sudo"
-  exit 1
-fi
 
 # Install necessary packages
 apt-get update && apt-get install -y gnupg wget
